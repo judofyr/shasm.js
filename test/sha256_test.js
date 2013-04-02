33fe62dffaf09245b0e56691905ba9f9736f74e8
Magnus Holm <judofyr@gmail.com>
2013-04-02 20:13:44 +0200

    Fix QUnit expected/actual

--- test/sha256_test.js
+++ test/sha256_test.js
@@ -5,21 +5,21 @@ QUnit.testStart(function() {
 });
 
 test("empty string", function() {
-  equal("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", s.final());
+  equal(s.final(), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
 });
 
 test("less than one block", function() {
   s.update("Hello world!");
-  equal("c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a", s.final());
+  equal(s.final(), "c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a");
 });
 
 test("exactly one block", function() {
   s.update("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-  equal("ffe054fe7ae0cb6dc65c3af9b61d5209f439851db43d0ba5997337df154668eb", s.final());
+  equal(s.final(), "ffe054fe7ae0cb6dc65c3af9b61d5209f439851db43d0ba5997337df154668eb");
 });
 
 test("multiple updates", function() {
   s.update("Hello ");
   s.update("world!");
-  equal("c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a", s.final());
+  equal(s.final(), "c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a");
 });
